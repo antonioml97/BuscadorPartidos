359d61097d4201aa6200fd4b511a3ab75d854a0e
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-11 18:21:04 +0100

    Cambios en gulfile.js para instalar depencias

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var install = require('gulp-install');
 
 
 //Tarea para testear el proyecto.
 gulp.task('test', async function(){
   gulp.src('test')
   .pipe(mocha());
+});
+
+//Tarea para instalar las dependencias
+gulp.task('install', async function(){
+  gulp.src(['./package.json','./package-lock.json'])
+    .pipe(install());
 });
 
 gulp.task('default', gulp.series('test'));
