65fa95dc523a8cd3899e7af0663f50e86eae9060
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-25 19:13:13 +0100

    Arreglar  bot #26

--- functions/bot.js
+++ functions/bot.js
@@ -8,15 +8,15 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 bot.start((ctx) => ctx.reply(process.uptime()));
 bot.help(ctx => ctx.reply('/dondeJuega{NombreEquipo}{Jornada-1} \t Te dice en que estadio juega un eqipo en una jornada' ))
 
-bot.command('Prueba', async (ctx) => {
-   // msg.chat.id se encarga de recoger el id del chat donde se está realizando la petición.
-   var chatId = msg.chat.id;
-   // msg.from.username se encarga de recoger el @alias del usuario.
-   var username = msg.from.username;
-   var res= "Hola, " + username + " soy un bot y te digo donde juega un equipo";
-   // Enviamos un mensaje indicando el id del chat, y concatenamos el nombre del usuario con nuestro saludo
-   ctx.reply(res)
-})
+// bot.command('Prueba', async (ctx) => {
+//    // msg.chat.id se encarga de recoger el id del chat donde se está realizando la petición.
+//    var chatId = msg.chat.id;
+//    // msg.from.username se encarga de recoger el @alias del usuario.
+//    var username = msg.from.username;
+//    var res= "Hola, " + username + " soy un bot y te digo donde juega un equipo";
+//    // Enviamos un mensaje indicando el id del chat, y concatenamos el nombre del usuario con nuestro saludo
+//    ctx.reply(res)
+// })
 
 
 
