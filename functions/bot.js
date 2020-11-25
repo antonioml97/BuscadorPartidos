423bd5b3583afcb1acfaeb58220dcac32c763a8f
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-25 18:50:17 +0100

    Arreglar  bot #26

--- functions/bot.js
+++ functions/bot.js
@@ -5,9 +5,20 @@ const API_URL = process.env.API_URL;
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply(process.uptime()));
+//bot.start((ctx) => ctx.reply(process.uptime()));
 bot.help(ctx => ctx.reply('/dondeJuega{NombreEquipo}{Jornada-1} \t Te dice en que estadio juega un eqipo en una jornada' ))
 
+//Declaramos la funcion
+bot.onText(/^\/start/, function(msg){
+  // Imprimimos en consola el mensaje recibido.
+  console.log(msg);
+  // msg.chat.id se encarga de recoger el id del chat donde se está realizando la petición.
+  var chatId = msg.chat.id;
+  // msg.from.username se encarga de recoger el @alias del usuario.
+  var username = msg.from.username;
+  // Enviamos un mensaje indicando el id del chat, y concatenamos el nombre del usuario con nuestro saludo
+  bot.sendMessage(chatId, "Hola, " + username + " soy un bot y te digo donde juega un equipo");
+});
 
 // bot.command('Today', async (ctx) => {
 
@@ -34,8 +45,9 @@ exports.handler = async (event, ctx, callback) => {
   // let cadena = ""
   
   // if (text == "/dondeJuega"){
-	// 	cadena = "Falta un equipo";
+	// 	cadena = "Falta";
 	// 	cadena += "\n";
+	// 	cadena += "Por ejemplo, /puntosBilal";
 	// }
 
   await bot.handleUpdate(JSON.parse(event.body));
