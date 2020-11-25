185e56e223854b5e5127325629451eff48e29e4c
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-25 13:08:07 +0100

    Uso de Netlify #25

---
+++ functions/bot.js
@@ -0,0 +1,14 @@
+const Telegraf = require("telegraf");
+const fetch = require("node-fetch");
+
+const API_URL = process.env.API_URL;
+
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply(process.uptime()));
+
+
+exports.handler = async (event, ctx, callback) => {
+  await bot.handleUpdate(JSON.parse(event.body));
+  return callback(null, { statusCode: 200 });
+};
