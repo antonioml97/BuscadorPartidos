2ece359f75efef51d1f6b3f59f2cc60384a26afd
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-25 13:31:57 +0100

    Probamos bot #26

--- functions/bot.js
+++ functions/bot.js
@@ -5,11 +5,10 @@ const API_URL = process.env.API_URL;
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-
 bot.start((ctx) => ctx.reply(process.uptime()));
 
 
 exports.handler = async (event, ctx, callback) => {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return callback(null, { statusCode: 200 });
+  await bot.handleUpdate(JSON.parse(event.body));
+  return callback(null, { statusCode: 200 });
 };
