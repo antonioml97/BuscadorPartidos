3251e2b1bbab618c7d02ad7f91780cf3d677fab8
Antonio Martin <antoniomartinleon97@gmail.com>
2020-11-25 18:47:12 +0100

    Arreglar  bot #26

--- functions/bot.js
+++ functions/bot.js
@@ -5,35 +5,37 @@ const API_URL = process.env.API_URL;
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.hears(/^\/start/, function(msg){
-  var chatId = msg.chat.id;
-  // msg.from.username se encarga de recoger el @alias del usuario.
-  var username = msg.from.username;
-  // Enviamos un mensaje indicando el id del chat, y concatenamos el nombre del usuario con nuestro saludo
-  bot.sendMessage(chatId, "Hola, " + username + " soy un bot y se decirte donde juega un equipo de futbol");
-});
-bot.help(ctx => ctx.reply('/dondejuega{NombreEquipo}{Jornada-1} \t Te dice en que estadio juega un eqipo en una jornada' ))
+bot.start((ctx) => ctx.reply(process.uptime()));
+bot.help(ctx => ctx.reply('/dondeJuega{NombreEquipo}{Jornada-1} \t Te dice en que estadio juega un eqipo en una jornada' ))
 
 
-bot.hears(/\/dondejuega (.+)/, async (ctx) => {
-  const genre = ctx.message.text.substr(7);
-  const res = await (await fetch(API_URL + `/available/genre/${genre}`)).json();
-  bot.telegram.sendMessage(ctx.chat.id, res);
-});
+// bot.command('Today', async (ctx) => {
+
+//   let res = 'Los partidos que se juegan hoy son:\n'
+  
+//   const response = await axios.get(config.service.api_today)
+
+//   if ( response.data.length > 0){
+//       for( var i = 0; i < response.data.length ; i++){ 
+//         res +=  "" +response.data[i]._player1 + " vs " + response.data[i]._player2 + "\n";
+//       }
+
+//   }
+//   ctx.reply(res)
+// })
 
 
 exports.handler = async (event, ctx, callback) => {
-  // // Obtenemos el cuerpo del mensaje, y lo separamos en el chat y en el texto enviado
+  // Obtenemos el cuerpo del mensaje, y lo separamos en el chat y en el texto enviado
 
 	// let msg = JSON.parse(event.body);
 	// let text = msg.message.text;
 	// let chat_id = msg.message.chat.id;
   // let cadena = ""
   
-  // if (text == "/dondejuega"){
-	// 	cadena = "Falta un equipo y una jornada";
+  // if (text == "/dondeJuega"){
+	// 	cadena = "Falta un equipo";
 	// 	cadena += "\n";
-	// 	cadena += "Por ejemplo, /dondejuegaGranadaJornada-1";
 	// }
 
   await bot.handleUpdate(JSON.parse(event.body));
